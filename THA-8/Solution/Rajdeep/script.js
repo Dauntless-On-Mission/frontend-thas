7a0356dbcf43e74345c40f611d292f16ea4879bf
Rajdeep Banik <rajdeepbanik2092001@gmail.com>
2021-07-01 19:27:05 +0530

    Delete THA-8/Solution/Rajdeep directory

--- THA-8/Solution/Rajdeep/script.js
+++
@@ -1,8 +0,0 @@
-const press = document.querySelectorAll('.childbox');
-
-
-press.forEach(item => {
-    item.addEventListener('click', event => {
-        item.classList.toggle("white")
-    })
-  })
