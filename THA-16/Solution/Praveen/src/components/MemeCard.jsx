3a728609fa17b0c81efba9c06e9eaa3adc98038b
Praveen-Prajapati <80213346+Praveen-Prajapati-2150@users.noreply.github.com>
2021-07-03 20:32:38 +0530

    tha-15 of chessBoard and tha-16 of memeCard in that ReactFile (#114)

---
+++ THA-16/Solution/Praveen/src/components/MemeCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./MemeCard.css";
+
+function Card() {
+  return (
+    <>
+      <div className="cardBase">
+        <img  alt="" />
+        <h1>Meme Card</h1>
+        <h3>There is your meme card</h3>
+      </div>
+    </>
+  );
+}
+
+
+export default Card;
