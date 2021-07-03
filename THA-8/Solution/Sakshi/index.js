c570562f588ef92a82fc04f2d322a4102515d1c0
Sakshi Saxena <btbte19107_sakshi@banasthali.in>
2021-07-03 23:09:04 +0530

    ✅ Sakshi:THA-8 (#116)

---
+++ THA-8/Solution/Sakshi/index.js
@@ -0,0 +1,27 @@
+const board = document.querySelector("#board");
+console.log(board);
+
+const CELL_COUNT = 100;
+const state = {};
+
+for (let i = 0; i < CELL_COUNT; i = i + 1) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.addEventListener("click", e => handleClick(e, i));
+  board.appendChild(cell);
+}
+
+console.log(board);
+
+const handleClick = (e, index) => {
+  const element = e.target;
+  if (state[index]) {
+    element.style.background = "purple";
+    delete state[index];
+  } 
+  else {
+    state[index] = "selected";
+    element.style.background = "rgb(154, 154, 243)";
+  }
+};
+
