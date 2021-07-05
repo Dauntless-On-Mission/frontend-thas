da9a4717ca999e58effcadad7d90954c9eb61c25
HardikAgrwl <hardikagarwal213227@gmail.com>
2021-07-05 20:00:41 +0530

    ✅ Hardik : THA-10

---
+++ THA-10/Solution/Hardik/src/cardArray.js
@@ -0,0 +1,50 @@
+import adobe from "./images/adobe-2.svg";
+import amazon from "./images/amazon-icon-1.svg";
+import google from "./images/google-2015.svg";
+import apple from "./images/apple1.svg";
+import cisco from "./images/cisco-2.svg";
+import facebook from "./images/facebook-3.svg";
+import flipkart from "./images/flipkart.svg";
+import uber from "./images/uber-15.svg";
+
+export const CardArray = [
+  {
+    type: "Adobe",
+    image: adobe,
+  },
+
+  {
+    type: "Amazon",
+    image: amazon,
+  },
+
+  {
+    type: "American Expresss",
+    image: google,
+  },
+
+  {
+    type: "Apple",
+    image: apple,
+  },
+
+  {
+    type: "Cisco",
+    image: cisco,
+  },
+
+  {
+    type: "Facebook",
+    image: facebook,
+  },
+
+  {
+    type: "Flipkart",
+    image: flipkart,
+  },
+
+  {
+    type: "Goldman Sachs",
+    image: uber,
+  },
+];
