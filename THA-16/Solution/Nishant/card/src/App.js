bfc3dc7d98e34b6b0a20d6b3bde6b2738831e772
Nishant Choudhary <69012179+N9shant@users.noreply.github.com>
2021-07-08 19:05:10 +0530

    ✅ Nishant: THA16(1) (#126)

---
+++ THA-16/Solution/Nishant/card/src/App.js
@@ -0,0 +1,13 @@
+import './Styles/App.css';
+import Card from './Components/Card.js'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
