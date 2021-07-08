89ec86223ddf9d2f006127bce15309fdfadee1f0
Nishant <choudhary123nishant@gmail.com>
2021-07-08 19:02:40 +0530

    ✅ Nishant: THA16(1)

---
+++ THA-16/Solution/Nishant/card/src/Components/Card.js
@@ -0,0 +1,14 @@
+import '../Styles/card.css'
+
+function card() {
+    return(
+    <div className="card">
+        <img src="https://images.unsplash.com/photo-1534859219971-7ff6d064333e?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8NnNNVmpUTFNrZVF8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Nature" />
+
+        <h1>Nature Card</h1>
+        <p>Nature Cards created for THA</p> 
+    </div>
+    );
+}
+
+export default card;
