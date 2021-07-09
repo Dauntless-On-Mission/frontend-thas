ed1769a41e1c603346eeaec5ebf78e24a678e941
Srujan Patluri <76472922+DevSrujan43@users.noreply.github.com>
2021-07-09 20:38:51 +0530

    ✔Srujan:THA-16 (#129)

---
+++ THA-16/Solution/srujan/src/Meme.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const Meme = () => {
+    return (
+        <div className = "memeCard">
+            <div className="card">
+                <img src="https://i.redd.it/jbnykv3de3a71.jpg" alt=""  height = "500px" width = "auto"/>
+            </div>
+            <div className="text">
+                <p> Meme card</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Meme
