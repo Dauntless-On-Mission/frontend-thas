372dd2ac621460d5583cb6c92d4ecce6162804a3
mercurial_harsh <harshguptaucme@gmail.com>
2021-07-11 21:33:12 +0530

    ✅ tha 14 15 16 done

---
+++ THA-16/Solution/Harsh/chessboard/src/Oddrow.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Oddrow = () => {
+    return (
+    <div className="oddRow">
+      <div className="square black"></div>
+      <div className="square"></div>
+      <div className="square black"></div>
+      <div className="square"></div>
+      <div className="square black"></div>
+      <div className="square"></div>
+      <div className="square black"></div>
+      <div className="square"></div>
+    </div>
+    )
+}
+
+export default Oddrow
