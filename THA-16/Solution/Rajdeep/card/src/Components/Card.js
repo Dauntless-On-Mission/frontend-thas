0570b77b905de62094ec6693213a653aca836f49
Rajdeep banik <rajdeepbanik2092001@gmail.com>
2021-07-12 23:50:20 +0530

    ✅THA-16 done

---
+++ THA-16/Solution/Rajdeep/card/src/Components/Card.js
@@ -0,0 +1,14 @@
+import '../Styles/card.css'
+
+function card() {
+    return(
+    <div className="card">
+        <img src="https://www.icegif.com/wp-content/uploads/dogecoin-icegif-8.gif" alt="doge" />
+
+        <h1>Doge Card</h1>
+        <p>Doge Is my Favourite memes</p> 
+    </div>
+    );
+}
+
+export default card;
