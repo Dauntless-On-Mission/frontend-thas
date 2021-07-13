5e2fac0d922d04645ba35bd4aed3e3a54fe78050
Sakshi Saxena <btbte19107_sakshi@banasthali.in>
2021-07-13 22:46:10 +0530

    ✅ Sakshi: tha-16-revised (#155)

---
+++ THA-16/Solution/Sakshi/memecard/src/Meme.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const Meme = () => {
+    return (
+        <div className = "memeCard">
+            <div className="card">
+                <img src="https://i.pinimg.com/originals/8f/35/28/8f35281e742e71be09697e7e6789b819.png" alt=""  height = "500px" width = "400px"/>
+            </div>
+            <div className="text">
+                <p> Meme card</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Meme
