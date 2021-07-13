84fbc3778ac225ed7e7889ec262e47d0a4a5bb74
sakshiisaxena <btbte19107_sakshi@banasthali.in>
2021-07-13 22:44:56 +0530

    ✅ Sakshi: tha-16-revised

---
+++ THA-16/Solution/Sakshi/chess/src/App.js
@@ -0,0 +1,22 @@
+import Rows from "./Rows";
+import Rows2 from "./Rows2";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Rows/>
+      <Rows2/>
+      <Rows/>
+      <Rows2/>
+      <Rows/>
+      <Rows2/>
+      <Rows/>
+      <Rows2/>
+      <Rows/>
+      <Rows2/>
+    </div>
+  );
+}
+
+export default App;
