3ace0fdbbedb931efbf4e142d443a67caeb6ebf2
Sakshi Saxena <btbte19107_sakshi@banasthali.in>
2021-07-16 17:46:00 +0530

    ✅ Sakshi:THA-17 (#158)

---
+++ THA-17/Solution/Sakshi/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CaloriePage from './chart';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <CaloriePage />
+    </div>
+  );
+}
+
+export default App;
